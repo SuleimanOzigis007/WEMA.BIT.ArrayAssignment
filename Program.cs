fc82be9674598d2678acd473198645fa0a8ca3b1
faisl <faisal@gmail.com>
2023-11-22 15:12:33 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿
+namespace WEMA.BIT.Array
+{
+    public class Array
+    {
+        public static void Main(string[] args)
+        {
+            string[] students = new string[10] { "Chibuzor", "Timmy", "Rita", "Tbams", "Esther", "Frank", "Grace", "Hannah", "Jane", "Jide" };
+
+            List<int> studentAges = new List<int> { 20, 18, 22, 19, 18, 21, 23, 20, 18, 24 };
+
+            bool isEighteen = studentAges.Contains(18);
+
+            // isEighteen = studentAges.Any(age => age == 18);
+
+            if (isEighteen)
+            {
+                Console.WriteLine("There is at least one student whose age is 18 in the list.");
+            }
+            else
+            {
+                Console.WriteLine("No student with age 18 found in the list.");
+            }
+        }
+    }
+}
